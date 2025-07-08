7b93c552adbc3d00c9bdeaf35fa8d5c18cb00cb9
Philipp Rich <skorphil@gmail.com>
2025-07-08 21:32:20 +0400

    fixed path resolution

--- src/composition.ts
+++ src/composition.ts
@@ -1,14 +1,15 @@
-import { NotebookController, TauriNotebookRepository } from "@adapters";
+import { NotebookController, TauriNotebookRepository } from "@adapters"; // TS module "/Users/philipp/Documents/GitHub/clean-architecture-feature/src/2-adapters/index"
 import { GetNotebook } from "@application";
 import { tauriFileReader } from "@frameworks/services";
 
 /*
-This is example of manual dependency injection.
+This is an example of manual dependency injection.
 
 Automatic dependency injection techniques, like dependency injection container 
 might be used. 
 
-For js/ts there are inversifyJs and ts-loader
+For js/ts there are inversifyJs and ts-loader libraries for automatic
+dependency injection
 */
 
 const notebookRepository = new TauriNotebookRepository(tauriFileReader);
